14e299bfd5dd7d53e16c58a4c0b92f2a6806cc5b
jeff <jeff.etale@student.moringaschool.com>
2023-07-09 22:40:29 +0300

    Done lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const init = () => {
+    const inputForm = document.querySelector('form');
   
-}
-
-document.addEventListener('DOMContentLoaded', init);
+    inputForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const input = document.querySelector('input#searchByID');
+      const movieDetails = document.querySelector('#movieDetails');
+  
+      fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+          if (data.error) {
+            movieDetails.innerHTML = `<p>No movie found with ID ${input.value}</p>`;
+          } else {
+            movieDetails.innerHTML = `
+              <h4>${data.title}</h4>
+              <p>${data.summary}</p>
+            `;
+          }
+        });
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', init);
+  
